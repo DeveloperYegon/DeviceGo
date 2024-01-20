60be2a00e886a32eed893613b0d13847e5df04d0
developeryegon <gideonyegon404@gmail.com>
2024-01-20 16:18:02 +0300

    addition of functionalities and styling

---
+++ src/assets/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <p>&copy; 2024, DeviceGo Stores. All rights are reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
